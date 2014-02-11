2fc1f0a4cc070bf1e5eb4f92b28ff96a5ecabe91
petertbernhardt <petertbernhardt@gmail.com>
2014-02-11 15:30:10 -0800

    Added a ball with physics. Chooses a random direction to bounce in and bounces off the paddles.

---
+++ Assets/BallControl.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+function Start () {
+	var randomNum = Random.Range(0, 2);
+	if (randomNum <= 0.5) {
+		// simple code, can change later
+		rigidbody2D.AddForce(new Vector2(80, 10));
+	} else {
+		// simple code, can change later
+		rigidbody2D.AddForce(new Vector2(-80, -10));
+	}
+}
+
+function OnCollisionEnter2D (colInfo : Collision2D) {
+	if (colInfo.collider.tag == "Player") {
+		Debug.Log("It's working!");
+	}
+}
