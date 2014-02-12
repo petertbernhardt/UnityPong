04c386688bce349e33054227ce912edf40ace6bd
Peter Thomas Bernhardt <petertbernhardt@gmail.com>
2014-02-11 22:48:12 -0800

    Changed the style of the GUI labels for the score.

--- Assets/GameManager.js
+++ Assets/GameManager.js
@@ -3,6 +3,8 @@
 static var player1Score : int = 0;
 static var player2Score : int = 0;
 
+var layout : GUISkin;
+
 static function Score (wallName : String) {
 	if (wallName == "rightWall") {
 		player1Score += 1;
@@ -12,6 +14,7 @@ static function Score (wallName : String) {
 }
 
 function OnGUI() {
-	GUI.Label(new Rect(Screen.width/2-150-12, 20, 100, 100), "" + player1Score);
-	GUI.Label(new Rect(Screen.width/2+150-12, 20, 100, 100), "" + player2Score);
+	GUI.skin = layout;
+	GUI.Label(new Rect(Screen.width/2-150-12, 25, 100, 100), "" + player1Score);
+	GUI.Label(new Rect(Screen.width/2+150-12, 25, 100, 100), "" + player2Score);
 }
