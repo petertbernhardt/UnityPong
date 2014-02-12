e691b778a256fb9ca2ca96485382c02b93873069
Peter Thomas Bernhardt <petertbernhardt@gmail.com>
2014-02-11 22:19:05 -0800

    Added scoring and GUI to show player score.

---
+++ Assets/Pong/GameManager.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+static var player1Score : int = 0;
+static var player2Score : int = 0;
+
+var layout : GUISkin;
+
+static function Score (wallName : String) {
+	if (wallName.Equals("rightWall")) {
+		player1Score += 1;
+	} else {
+		player2Score += 1;
+	}
+	Debug.Log("Player Score 1 is " + player1Score);
+	Debug.Log("Player Score 2 is " + player2Score);
+}
+
+function OnGUI () {
+	GUI.skin = layout;
+	GUI.Label(new Rect(Screen.width / 2 - 150-12, 20, 100, 100), "" + player1Score);
+	GUI.Label(new Rect(Screen.width / 2 + 150-12, 20, 100, 100), "" + player2Score);
+}
---
+++ Assets/Pong/SideWalls.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+
+function OnTriggerEnter2D (hitInfo : Collider2D) {
+	if (hitInfo.name.Equals("Ball")) {
+		var wallName = transform.name;
+		GameManager.Score(wallName);
+	}
+}
